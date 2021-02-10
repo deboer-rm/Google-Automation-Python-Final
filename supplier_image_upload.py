6a1099e81e1aa5cd0023616c8a83f0475017d55d
deboer-rm <77012413+deboer-rm@users.noreply.github.com>
2021-02-10 10:31:49 -0500

    Update supplier_image_upload.py
    Fixed some syntax errors with the folder paths.

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -6,16 +6,16 @@ import sys
 
 # Initiate variables for source and upload path.
 
-source_files = os.listdir('/supplier-data/images')
+source_path = 'supplier-data/images/'
+source_full = os.listdir(source_path)
 upload_path = 'http://localhost/upload/'
 
 # Begin the loop to open and iterate through all the new jpeg images in the source folder.
 # The loop will then make sure just to open the jpeg files.
 # It will then upload them to the correct upload path
 
-for file in source_files:
+for file in source_full:
   if file.endswith('.jpeg'):
-    temp = open(source + file)
-    response = requests.post(upload_path, files=temp)
-    #print(response.status_code)
-    temp.close()
+    with open(source_path + file, 'rb') as opened:
+      response = requests.post(upload_path, files={'file': opened})
+      #print(response.status_code)
