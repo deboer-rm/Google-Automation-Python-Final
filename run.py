b46e85deda6e9214a03514965450c748aa6c2c26
deboer-rm <77012413+deboer-rm@users.noreply.github.com>
2021-02-10 10:52:57 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,16 +4,16 @@ import os
 import requests
 import sys
 
-# Initiate variables to set path and external IP
+# Initiate variables for source and upload path.
 
 source_path = 'supplier-data/descriptions'
 source_full = os.listdir(source_path)
 upload_path = 'http://localhost/fruits/'
 
 # Begin the loop to open and iterate through all files in the source folder.
-# The loop will then make sure not to open any strange temp files.
+# The loop will then make sure not to open anything other than a txt file.
 # It will open them, read them, and split the new lines for our dictionary.
-# Finally, it will post the review and grab a http status code.
+# Finally, it will post the information and grab a http status code.
 
 for file in source_full:
   if file.endswith('.txt'):
